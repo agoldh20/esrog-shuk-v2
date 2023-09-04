0302e357f20c75b27c2c10eb0fbb0d9f38cdecd5
Adam Goldwater <adam.goldwater@wnco.com>
2023-09-04 14:42:44 -0500

    config for deploy

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -16,7 +16,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   end
 
   allow do
-    origins "https://link-to-production-app.com/"
+    origins "esrog-shuk.onrender.com"
 
     resource "*",
              headers: :any,
