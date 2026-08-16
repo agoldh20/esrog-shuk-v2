78f84dabc75764aae53f4266fa117d63be847862
agoldh20 <agoldh20@gmail.com>
2026-08-16 14:27:13 -0500

    db entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,15 +7,14 @@ rm -f /app/tmp/pids/server.pid
 if [ -n "$DATABASE_URL" ]; then
   echo "Waiting for PostgreSQL database connection..."
   
-  # Wait for Postgres port to be open
   until pg_isready -h db -p 5432; do
     echo "PostgreSQL is booting... retrying in 2 seconds"
     sleep 2
   done
 
-  echo "PostgreSQL port is ready! Preparing database schema & migrations..."
-  bundle exec rails db:prepare || bundle exec rails db:migrate
-  echo "Database is ready and up to date."
+  echo "PostgreSQL port is ready! Loading schema and running migrations..."
+  bundle exec rails db:schema:load || bundle exec rails db:migrate
+  echo "Database schema loaded and migrations are up to date."
 fi
 
 # Then exec the container's main process (CMD in Dockerfile).
