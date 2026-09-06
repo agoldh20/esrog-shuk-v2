a0eaaff425cb1b64dd5a0558905d7eb3205e08c1
agoldh20 <agoldh20@gmail.com>
2026-09-06 13:37:14 -0500

    move to render

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,7 +3,4 @@
 set -o errexit
 
 bundle install
-#bundle exec rake assets:precompile
-#bundle exec rake assets:clean
-#bundle exec rails db:schema:load DISABLE_DATABASE_ENVIRONMENT_CHECK=1
-bundle exec rails db:migrate
+bundle exec rails db:schema:load || bundle exec rails db:migrate
